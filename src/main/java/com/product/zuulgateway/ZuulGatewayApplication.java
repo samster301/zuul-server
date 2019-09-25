7adba1781020dcae4c8dc2f66591666fcaaa184c
e034391 <arunabee@gmail.com>
2019-09-24 20:20:49 -0500

    changes for price service

--- src/main/java/com/product/zuulgateway/ZuulGatewayApplication.java
+++ src/main/java/com/product/zuulgateway/ZuulGatewayApplication.java
@@ -1,13 +1,23 @@
 package com.product.zuulgateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Autowired
+	public SimpleFilter simpleFilter(){
+		return new SimpleFilter();
 	}
 
 }
